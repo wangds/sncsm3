7c24efa4d3abd2dba437798fb4662d1d983f1097
David Wang <millimillenary@gmail.com>
2010-06-10 06:37:33 +0000

    Add main font (6x12, lower case).

---
+++ src/table/font.c
@@ -0,0 +1,397 @@
+/* font.c
+ *
+ * Font from Summon Night: Swordcraft Story 2.
+ * Each letter is 6x12.
+ */
+
+#include "table/font.h"
+
+const char k_font_lower_a[12][6] = {
+	"      ",
+	"      ",
+	"      ",
+	"      ",
+	"xxxx  ",
+	"    x ",
+	" xxxx ",
+	"x   x ",
+	"x   x ",
+	" xxxx ",
+	"      ",
+	"      "
+};
+
+const char k_font_lower_b[12][6] = {
+	"      ",
+	"x     ",
+	"x     ",
+	"x     ",
+	"xxxx  ",
+	"x   x ",
+	"x   x ",
+	"x   x ",
+	"x   x ",
+	"xxxx  ",
+	"      ",
+	"      "
+};
+
+const char k_font_lower_c[12][6] = {
+	"      ",
+	"      ",
+	"      ",
+	"      ",
+	" xxx  ",
+	"x   x ",
+	"x     ",
+	"x     ",
+	"x   x ",
+	" xxx  ",
+	"      ",
+	"      "
+};
+
+const char k_font_lower_d[12][6] = {
+	"      ",
+	"    x ",
+	"    x ",
+	"    x ",
+	" xxxx ",
+	"x   x ",
+	"x   x ",
+	"x   x ",
+	"x   x ",
+	" xxxx ",
+	"      ",
+	"      "
+};
+
+const char k_font_lower_e[12][6] = {
+	"      ",
+	"      ",
+	"      ",
+	"      ",
+	" xxx  ",
+	"x   x ",
+	"xxxxx ",
+	"x     ",
+	"x   x ",
+	" xxx  ",
+	"      ",
+	"      "
+};
+
+const char k_font_lower_f[12][6] = {
+	"      ",
+	"   xx ",
+	"  x   ",
+	"  x   ",
+	"xxxxx ",
+	"  x   ",
+	"  x   ",
+	"  x   ",
+	"  x   ",
+	"  x   ",
+	"      ",
+	"      "
+};
+
+const char k_font_lower_g[12][6] = {
+	"      ",
+	"      ",
+	"      ",
+	"      ",
+	" xxxx ",
+	"x  x  ",
+	"x  x  ",
+	" xx   ",
+	"x     ",
+	"xxxx  ",
+	"x   x ",
+	" xxx  "
+};
+
+const char k_font_lower_h[12][6] = {
+	"      ",
+	"x     ",
+	"x     ",
+	"x     ",
+	"xxxx  ",
+	"x   x ",
+	"x   x ",
+	"x   x ",
+	"x   x ",
+	"x   x ",
+	"      ",
+	"      "
+};
+
+const char k_font_lower_i[12][6] = {
+	"      ",
+	"  x   ",
+	"  x   ",
+	"      ",
+	"  x   ",
+	"  x   ",
+	"  x   ",
+	"  x   ",
+	"  x   ",
+	"  x   ",
+	"      ",
+	"      "
+};
+
+const char k_font_lower_j[12][6] = {
+	"      ",
+	"  x   ",
+	"  x   ",
+	"      ",
+	"  x   ",
+	"  x   ",
+	"  x   ",
+	"  x   ",
+	"  x   ",
+	"  x   ",
+	"  x   ",
+	"xx    "
+};
+
+const char k_font_lower_k[12][6] = {
+	"      ",
+	"x     ",
+	"x     ",
+	"x     ",
+	"x  x  ",
+	"x x   ",
+	"xx    ",
+	"x x   ",
+	"x  x  ",
+	"x   x ",
+	"      ",
+	"      "
+};
+
+const char k_font_lower_l[12][6] = {
+	"      ",
+	"  x   ",
+	"  x   ",
+	"  x   ",
+	"  x   ",
+	"  x   ",
+	"  x   ",
+	"  x   ",
+	"  x   ",
+	"  x   ",
+	"      ",
+	"      "
+};
+
+const char k_font_lower_m[12][6] = {
+	"      ",
+	"      ",
+	"      ",
+	"      ",
+	"xxxx  ",
+	"x x x ",
+	"x x x ",
+	"x x x ",
+	"x x x ",
+	"x x x ",
+	"      ",
+	"      "
+};
+
+const char k_font_lower_n[12][6] = {
+	"      ",
+	"      ",
+	"      ",
+	"      ",
+	"xxxx  ",
+	"x   x ",
+	"x   x ",
+	"x   x ",
+	"x   x ",
+	"x   x ",
+	"      ",
+	"      "
+};
+
+const char k_font_lower_o[12][6] = {
+	"      ",
+	"      ",
+	"      ",
+	"      ",
+	" xxx  ",
+	"x   x ",
+	"x   x ",
+	"x   x ",
+	"x   x ",
+	" xxx  ",
+	"      ",
+	"      "
+};
+
+const char k_font_lower_p[12][6] = {
+	"      ",
+	"      ",
+	"      ",
+	"      ",
+	"xxxx  ",
+	"x   x ",
+	"x   x ",
+	"x   x ",
+	"x   x ",
+	"xxxx  ",
+	"x     ",
+	"x     "
+};
+
+const char k_font_lower_q[12][6] = {
+	"      ",
+	"      ",
+	"      ",
+	"      ",
+	" xxxx ",
+	"x   x ",
+	"x   x ",
+	"x   x ",
+	"x   x ",
+	" xxxx ",
+	"    x ",
+	"    x "
+};
+
+const char k_font_lower_r[12][6] = {
+	"      ",
+	"      ",
+	"      ",
+	"      ",
+	"x xxx ",
+	"xx    ",
+	"x     ",
+	"x     ",
+	"x     ",
+	"x     ",
+	"      ",
+	"      "
+};
+
+const char k_font_lower_s[12][6] = {
+	"      ",
+	"      ",
+	"      ",
+	"      ",
+	" xxx  ",
+	"x   x ",
+	" xx   ",
+	"   x  ",
+	"x   x ",
+	" xxx  ",
+	"      ",
+	"      "
+};
+
+const char k_font_lower_t[12][6] = {
+	"      ",
+	"      ",
+	"  x   ",
+	"  x   ",
+	"xxxxx ",
+	"  x   ",
+	"  x   ",
+	"  x   ",
+	"  x   ",
+	"   xx ",
+	"      ",
+	"      "
+};
+
+const char k_font_lower_u[12][6] = {
+	"      ",
+	"      ",
+	"      ",
+	"      ",
+	"x   x ",
+	"x   x ",
+	"x   x ",
+	"x   x ",
+	"x  xx ",
+	" xx x ",
+	"      ",
+	"      "
+};
+
+const char k_font_lower_v[12][6] = {
+	"      ",
+	"      ",
+	"      ",
+	"      ",
+	"x   x ",
+	"x   x ",
+	"x   x ",
+	" x x  ",
+	" x x  ",
+	"  x   ",
+	"      ",
+	"      "
+};
+
+const char k_font_lower_w[12][6] = {
+	"      ",
+	"      ",
+	"      ",
+	"      ",
+	"x   x ",
+	"x x x ",
+	"x x x ",
+	"x x x ",
+	"x x x ",
+	" x x  ",
+	"      ",
+	"      "
+};
+
+const char k_font_lower_x[12][6] = {
+	"      ",
+	"      ",
+	"      ",
+	"      ",
+	"x   x ",
+	" x x  ",
+	"  x   ",
+	"  x   ",
+	" x x  ",
+	"x   x ",
+	"      ",
+	"      "
+};
+
+const char k_font_lower_y[12][6] = {
+	"      ",
+	"      ",
+	"      ",
+	"      ",
+	"x   x ",
+	"x   x ",
+	"x   x ",
+	" x x  ",
+	" x x  ",
+	"  x   ",
+	"  x   ",
+	"xx    "
+};
+
+const char k_font_lower_z[12][6] = {
+	"      ",
+	"      ",
+	"      ",
+	"      ",
+	"xxxxx ",
+	"    x ",
+	"   x  ",
+	"  x   ",
+	" x    ",
+	"xxxxx ",
+	"      ",
+	"      "
+};
---
+++ src/table/font.h
@@ -0,0 +1,31 @@
+#ifndef TABLE_FONT_H
+#define TABLE_FONT_H
+
+extern const char k_font_lower_a[12][6];
+extern const char k_font_lower_b[12][6];
+extern const char k_font_lower_c[12][6];
+extern const char k_font_lower_d[12][6];
+extern const char k_font_lower_e[12][6];
+extern const char k_font_lower_f[12][6];
+extern const char k_font_lower_g[12][6];
+extern const char k_font_lower_h[12][6];
+extern const char k_font_lower_i[12][6];
+extern const char k_font_lower_j[12][6];
+extern const char k_font_lower_k[12][6];
+extern const char k_font_lower_l[12][6];
+extern const char k_font_lower_m[12][6];
+extern const char k_font_lower_n[12][6];
+extern const char k_font_lower_o[12][6];
+extern const char k_font_lower_p[12][6];
+extern const char k_font_lower_q[12][6];
+extern const char k_font_lower_r[12][6];
+extern const char k_font_lower_s[12][6];
+extern const char k_font_lower_t[12][6];
+extern const char k_font_lower_u[12][6];
+extern const char k_font_lower_v[12][6];
+extern const char k_font_lower_w[12][6];
+extern const char k_font_lower_x[12][6];
+extern const char k_font_lower_y[12][6];
+extern const char k_font_lower_z[12][6];
+
+#endif
