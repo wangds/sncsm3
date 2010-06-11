07378597d98ab5cbb72e47fdd94949ecfc2e7b99
David Wang <millimillenary@gmail.com>
2010-06-11 03:24:27 +0000

    Add main font (6x12, upper case).

--- src/table/font.h
+++ src/table/font.h
@@ -1,6 +1,33 @@
 #ifndef TABLE_FONT_H
 #define TABLE_FONT_H
 
+extern const char k_font_upper_a[12][6];
+extern const char k_font_upper_b[12][6];
+extern const char k_font_upper_c[12][6];
+extern const char k_font_upper_d[12][6];
+extern const char k_font_upper_e[12][6];
+extern const char k_font_upper_f[12][6];
+extern const char k_font_upper_g[12][6];
+extern const char k_font_upper_h[12][6];
+extern const char k_font_upper_i[12][6];
+extern const char k_font_upper_j[12][6];
+extern const char k_font_upper_k[12][6];
+extern const char k_font_upper_l[12][6];
+extern const char k_font_upper_m[12][6];
+extern const char k_font_upper_n[12][6];
+extern const char k_font_upper_o[12][6];
+extern const char k_font_upper_p[12][6];
+extern const char k_font_upper_q[12][6];
+extern const char k_font_upper_r[12][6];
+extern const char k_font_upper_s[12][6];
+extern const char k_font_upper_t[12][6];
+extern const char k_font_upper_u[12][6];
+extern const char k_font_upper_v[12][6];
+extern const char k_font_upper_w[12][6];
+extern const char k_font_upper_x[12][6];
+extern const char k_font_upper_y[12][6];
+extern const char k_font_upper_z[12][6];
+
 extern const char k_font_lower_a[12][6];
 extern const char k_font_lower_b[12][6];
 extern const char k_font_lower_c[12][6];
