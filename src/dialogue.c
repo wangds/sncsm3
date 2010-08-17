5438454e91b0f19f5b22ac256061301e012e075a
David Wang <millimillenary@gmail.com>
2010-08-17 10:05:20 +1000

    Automatically mark in addresses (with some guessing).

--- src/dialogue.c
+++ src/dialogue.c
@@ -276,6 +276,135 @@ regenerate_dialogue(void)
 	}
 }
 
+static bool
+is_valid_jump_addr(const dlg_t *c)
+{
+	const dlg_t *nx;
+	const dlg_t *dest;
+	int addr, cmd;
+
+	if (c == NULL)
+		return false;
+
+	nx = c->next;
+	if (nx == NULL)
+		return false;
+
+	addr = (nx->d.raw << 8) | (c->d.raw);
+
+	/* Odd addresses don't happen! */
+	if (addr & 0x01)
+		return false;
+
+	/* Usually we jump to later addresses. */
+	if (addr <= c->index)
+		return false;
+
+	/* Make sure destination exists. */
+	for (dest = c; dest != NULL; dest = dest->next) {
+		if (dest->index == addr)
+			break;
+	}
+
+	if (dest == NULL)
+		return false;
+
+	nx = dest->next;
+	if (nx == NULL)
+		return false;
+
+	/* Check destination is not mid-sentence. */
+	cmd = (dest->d.raw << 8) | (nx->d.raw);
+	if (cmd >= 0x8000) {    /* Assume these are characters. */
+		const dlg_t *pv2 = dest->prev;
+		const dlg_t *pv1 = pv2->prev;
+
+		cmd = (pv1->d.raw << 8) | (pv2->d.raw);
+		if (cmd >= 0x8000) {
+			fprintf(stderr, "Jump mid sentence? addr==%02x%02x\n",
+					(addr & 0xff), (addr >> 8));
+			return false;
+		}
+	}
+
+	return true;
+}
+
+static dlg_t *
+proc_0200(dlg_t *c1)
+{
+	dlg_t *c[16];
+	int i;
+
+	/* Don't really understand this case too well...
+	 * Seems to be a jump if we find
+	 *
+	 *  02 00 xx xx -> c[2] c[3]  not odd
+	 *  ?? ?? ?? ?? -> c[4] c[5]  not 0x00 0x00 (usually 0x02 0x00 or 0x03 0x00)
+	 *  01 00 ?? ?? -> c[8] c[9]  is  0x01 0x00
+	 *  8- ?? ?? ?? -> c[12]c[13] is  0x87 0x00 or 0x8a 0x00 or 0x8b 0x00
+	 *
+	 * Care is required as there are 6 byte 02 00's, similar to 01 00.
+	 */
+
+	c[0] = c1;
+	for (i = 1; i < 16; i++) {
+		c[i] = c[i-1]->next;
+		if (c[i] == NULL)
+			return NULL;
+	}
+
+	if (is_valid_jump_addr(c[2])) {
+		dialogue_mark_address(c[2]);
+		return c[15]->next;
+	}
+	else {
+		return c[6];
+	}
+}
+
+static void
+preprocess_dialogue(void)
+{
+	dlg_t *c1, *c2;
+	dlg_t *nx;
+
+	for (c1 = s_dialogue_str; c1 != NULL; c1 = nx) {
+		/* We should have even length strings. */
+		c2 = c1->next;
+		assert(c2 != NULL);
+
+		switch ((c1->d.raw << 8) | (c2->d.raw)) {
+			case 0x0100:
+				nx = dlg_skip(c1, 6);
+				break;
+
+			case 0x0200:    /* jump if? */
+				nx = proc_0200(c1);
+				break;
+
+			case 0x0300:    /* goto */
+			case 0x0700:    /* gosub */
+			case 0x1303:    /* menu start? */
+			case 0x1403:    /* menu item. */
+				if (is_valid_jump_addr(c2->next)) {
+					dialogue_mark_address(c2->next);
+				}
+				else {
+					fprintf(stderr, "Preprocessor confused by 0x%02x 0x%02x\n",
+							c1->d.raw, c2->d.raw);
+				}
+
+				nx = dlg_skip(c1, 4);
+				break;
+
+			default:
+				nx = c2->next;
+				break;
+		}
+	}
+}
+
 void
 read_dialogue(const unsigned char *rom, int offset)
 {
@@ -336,6 +465,7 @@ read_dialogue(const unsigned char *rom, int offset)
 	}
 
 	regenerate_dialogue();
+	preprocess_dialogue();
 	fprintf(stdout, "Read ended at offset 0x%08x\n", offset);
 }
 
